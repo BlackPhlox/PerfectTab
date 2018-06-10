c51a24a1abec1d5fd02422b9f9b5e3c3ff3b489a
Mikkel Rasmussen <theluja@gmail.com>
2018-06-10 21:52:52 +0200

    First working example, removed currently unnessary code

--- background.js
+++ background.js
@@ -8,8 +8,21 @@ chrome.browserAction.onClicked.addListener(buttonClicked);
 
 function buttonClicked(tab) {
   let msg = {
-	txt: chrome.bookmarks.getTree()
-  };
-  chrome.tabs.sendMessage(tab.id, msg);
-  window.open('https://blackphlox.github.io/PerfectTab', '_blank');
+    txt: "Hello"
+  }
+
+  chrome.bookmarks.getTree((tree) => {
+    console.log(tree[0]);
+    msg.tree = tree[0];
+    chrome.tabs.sendMessage(tab.id,msg)
+  });
+
+  /*var newURL = "https://blackphlox.github.io/PerfectTab";
+  let newTab =chrome.tabs.create({ url: newURL });
+  */
 }
+
+
+
+//Get favicon
+//https://www.google.com/s2/favicons?domain=google.dk
--- content.js
+++ content.js
@@ -1,19 +1,43 @@
-// Daniel Shiffman
-// http://codingtra.in
-// http://patreon.com/codingtrain
 
-console.log("Chrome extension go?");
-
-
+console.log("New tab script started");
 
 chrome.runtime.onMessage.addListener(gotMessage);
 
 function gotMessage(message, sender, sendResponse) {
-  console.log(message.txt);
-  if (message.txt === "hello") {
-    let paragraphs = document.getElementsByTagName('p');
-    for (elt of paragraphs) {
-      elt.style['background-color'] = '#FF00FF';
-    }
+  console.log(message.tree);
+  printTree(message.tree);
+}
+
+let count = 0;
+
+function printTree(tree){
+  count++;
+  if(!tree.children){
+    count--;
+    return;
   }
+  for (let i = 0; i < tree.children.length; i++) {
+    let string = "";
+    string += tree.children[i].title;
+    if(tree.children[i].url) string += "\u00A0(" + tree.children[i].url +")"; 
+
+    printP(count,string);
+    printTree(tree.children[i]);
+    if(!tree.children[i].url) count--;
+  }
+}
+
+function printP(index,string){
+  if(!string){
+    return; 
+  }
+  let p = document.createElement("h"+index);
+  let space = "";
+  for(let i = 0; i < index; i++){
+    space += "\u00A0\u00A0\u00A0\u00A0";
+  }
+  if(index>0) space += "-\u00A0";
+  let text = document.createTextNode(space+string);
+  p.appendChild(text);
+  document.body.appendChild(p);
 }
