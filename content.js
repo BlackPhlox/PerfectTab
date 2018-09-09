bd1876e2766e6db3e2570f2446e847e5bebc7ad9
Mikkel Rasmussen <theluja@gmail.com>
2018-09-09 04:02:01 +0200

    ...

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 
-console.log("New tab script started");
+console.log("A new Perfect Tab script started");
 
 chrome.runtime.onMessage.addListener(gotMessage);
 
@@ -21,17 +21,24 @@ function printTree(tree){
     string += tree.children[i].title;
     if(tree.children[i].url) string += "\u00A0(" + tree.children[i].url +")"; 
 
-    printP(count,string);
+    printP(count,string,tree.children[i].url);
     printTree(tree.children[i]);
     if(!tree.children[i].url) count--;
   }
 }
 
-function printP(index,string){
+function printP(index,string,url){
   if(!string){
     return; 
   }
+  let div = document.createElement("div")
+  //div.style.display = "inline-block";
+  //div.style.float = "left";
+  let favIcon = document.createElement("img");
   let p = document.createElement("h"+index);
+    p.style.margin = 0;
+    p.style.display = "inline";
+    
   let space = "";
   for(let i = 0; i < index; i++){
     space += "\u00A0\u00A0\u00A0\u00A0";
@@ -39,5 +46,12 @@ function printP(index,string){
   if(index>0) space += "-\u00A0";
   let text = document.createTextNode(space+string);
   p.appendChild(text);
-  document.body.appendChild(p);
+    
+  //Defined
+  if(url)favIcon.src = 'chrome://favicon/' + url;
+  //else favIcon.src = "folderIcon"; To be implemented
+  
+  div.appendChild(favIcon);
+  div.appendChild(p);
+  document.body.appendChild(div);
 }
